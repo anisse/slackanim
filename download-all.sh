fd9672328c6d11fbc1c12f4533a339bcca9b77fd
Anisse Astier <anisse@astier.eu>
2025-11-29 18:57:34 +0100

    cleanup unused implementation

--- download-all.sh
+++ download-all.sh
@@ -48,26 +48,3 @@ jq -r '.icons[] |
         }[$match]
       )
   )' data/api.json | xargs -n2 /bin/bash -c "echo \$1; curl -LJ --silent --show-error --fail -o \"data/$DOWNLOAD_TYPE/\$1.$EXTENSION\" https://fonts.gstatic.com/s/e/notoemoji/latest/\$0/$FILENAME ; sleep 1"
-
-# jq -cr '.icons[] | [ .tags[0], .codepoint ]' < data/api.json \
-# 	| tr -d '":][!?' \
-# 	| tr " " "-" \
-# 	| tr "[:upper:]" "[:lower:]" \
-# 	| gawk -F "," '@load "ordchr"; {
-# 		tag = $1;
-# 		codepoint = $2;
-#
-# 		count = patsplit(codepoint, colors, /_1f3f./);
-# 		color = "";
-# 		for (i = 1; i <= count; i++) {
-# 			switch (colors[i]) {
-# 				case /_1f3fb/: color = color "-skin-2"; break;
-# 				case /_1f3fc/: color = color "-skin-3"; break;
-# 				case /_1f3fd/: color = color "-skin-4"; break;
-# 				case /_1f3fe/: color = color "-skin-5"; break;
-# 				case /_1f3ff/: color = color "-skin-6"; break;
-# 			}
-# 		}
-# 		outname = tag color "'".$EXTENSION"'";
-# 		system("echo "outname"; curl -LJ --silent --show-error --fail -o \"data/'"$DOWNLOAD_TYPE"'/"outname"\" https://fonts.gstatic.com/s/e/notoemoji/latest/"codepoint"/'"$FILENAME"' ; sleep 1");
-# }'
